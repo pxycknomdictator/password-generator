80bd97851b490fe0e6048fd6857c9f09def9e0a8
Noman <pxycknomdictator@outlook.com>
2024-10-27 00:11:41 +0500

    initial commit for folder structure

---
+++ README.md
@@ -0,0 +1,1 @@
+# Password Generator App Using React + Tailwindcss + TypeScript
---
+++ src/App.tsx
@@ -0,0 +1,5 @@
+export const App = () => (
+  <h1 className="text-3xl underline text-center">
+    PWD Generator With React + TypeScript
+  </h1>
+);
