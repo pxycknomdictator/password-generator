63e337e80db9325adc55df9c33ca14b62b1d986d
Noman <pxycknomdictator@outlook.com>
2024-10-27 15:37:09 +0500

    added styling in checkboxes

--- src/components/CheckBoxes.tsx
+++ src/components/CheckBoxes.tsx
@@ -2,23 +2,40 @@ import { FC } from "react";
 
 export const CheckBoxes: FC = () => {
   return (
-    <>
-      <div>
-        <input type="checkbox" name="symbols" id="symbols" />
-        <label htmlFor="symbols">Include Symbols</label>
-      </div>
-      <div>
-        <input type="checkbox" name="numbers" id="numbers" />
-        <label htmlFor="numbers">Include Numbers</label>
-      </div>
-      <div>
+    <section className="space-y-1 my-6">
+      <div className="space-x-3">
         <input
+          className="scale-110"
+          type="checkbox"
+          name="symbols"
+          id="symbols"
+        />
+        <label className="text-[1.1rem]" htmlFor="symbols">
+          Include Symbols
+        </label>
+      </div>
+      <div className="space-x-3">
+        <input
+          className="scale-110"
+          type="checkbox"
+          name="numbers"
+          id="numbers"
+        />
+        <label className="text-[1.1rem]" htmlFor="numbers">
+          Include Numbers
+        </label>
+      </div>
+      <div className="space-x-3">
+        <input
+          className="scale-110"
           type="checkbox"
           name="specialCharacters"
           id="specialCharacters"
         />
-        <label htmlFor="specialCharacters">Include Special Characters</label>
+        <label className="text-[1.1rem]" htmlFor="specialCharacters">
+          Include Special Characters
+        </label>
       </div>
-    </>
+    </section>
   );
 };
